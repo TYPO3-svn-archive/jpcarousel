4064d632137f319ace2cac6667943de927351b14
jacco <jacco@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-06 14:12:06 +0000

    Initial
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jpcarousel/trunk@52782 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "jpcarousel".
+#
+# Auto generated 08-09-2011 11:12
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'jpCarousel',
+	'description' => 'Carousel based on jQuery.carouFredSel. For images (photo gallery / slideshow), content elements and output of extensions like tt_news. Optional click enlarge with PMKshadowbox or RZcolorbox and optional T3 jQuery integration.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.1.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Jacco van der Post',
+	'author_email' => 'jacco@id-internetservices.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:28:{s:9:"ChangeLog";s:4:"5eff";s:9:"Changelog";s:4:"825b";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"5a8c";s:17:"ext_localconf.php";s:4:"cad9";s:14:"ext_tables.php";s:4:"bf30";s:19:"flexform_ds_pi1.xml";s:4:"d487";s:13:"locallang.xml";s:4:"0649";s:16:"locallang_db.xml";s:4:"a644";s:12:"t3jquery.txt";s:4:"2d52";s:14:"doc/manual.sxw";s:4:"b2b3";s:19:"doc/wizard_form.dat";s:4:"827e";s:20:"doc/wizard_form.html";s:4:"c088";s:14:"pi1/ce_wiz.gif";s:4:"74c7";s:31:"pi1/class.tx_jpcarousel_pi1.php";s:4:"b7a9";s:39:"pi1/class.tx_jpcarousel_pi1_wizicon.php";s:4:"ea35";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"c86b";s:17:"res/template.html";s:4:"9d16";s:22:"res/css/jpcarousel.css";s:4:"d5da";s:22:"res/img/caption_bg.png";s:4:"01f0";s:17:"res/img/clear.gif";s:4:"cc11";s:23:"res/img/left-button.png";s:4:"e2c1";s:24:"res/img/right-button.png";s:4:"fd4a";s:39:"res/scripts/jquery-1.4.2-1.8.0-1.1.2.js";s:4:"a8d7";s:40:"res/scripts/jquery.carouFredSel-1.3.3.js";s:4:"61dc";s:31:"static/jpcarousel/constants.txt";s:4:"e845";s:27:"static/jpcarousel/setup.txt";s:4:"574b";}',
+);
+
+?>
